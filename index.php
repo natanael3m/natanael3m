201a1ef9590732da2a51f78b8d30e44e5fdae100
Natanael Machado da Silva <natanael3m@gmail.com>
2020-09-27 12:43:01 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php>
+
+/*  TODO REVISAR CÁLCULO DE AMORTIZAÇÃO */
+/* FIXME CORRIGIR CÁLCULO DAS MÉDIAS DOS ALUNOS */
+
+/**@author Natanael3m <natanael3m@gmail.com> */
+phpinfo();
+
+/**
+ * Class constructor.
+ */
